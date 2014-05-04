5be0891451462a69314f2ddbdf7f5da0d063d201
griechel <griechel@stanford.edu>
2014-05-04 12:38:59 -0700

    Added carrier menu

--- Hudson/grails-app/controllers/home/HomeController.groovy
+++ Hudson/grails-app/controllers/home/HomeController.groovy
@@ -4,6 +4,7 @@ import HudsonJobs.*
 import hudson.User
 import java.security.MessageDigest
 import java.security.SecureRandom
+import javax.xml.bind.DatatypeConverter
 
 
 class HomeController {
@@ -30,17 +31,13 @@ class HomeController {
 		
 	}
 	
-	//User information and their first search query.
-	//JUST DO ONE FOR NOW!
 	def register(){}
-	
-	//def newuser() {}
-	
+		
 	def newusersession(){		
 		User usr = new User()
 		usr.salt = getSalt()
 		
-		usr.passwordHash = getHashedPassword(params.password + usr.salt) //hopefully this works?
+		usr.passwordHash = getHashedPassword(params.password, usr.salt) //hopefully this works?
 		usr.email = params.email
 		usr.firstName = params.firstName
 		usr.lastName = params.lastName
@@ -60,24 +57,17 @@ class HomeController {
 	
 	private static String getHashedPassword(String pass, String salt) {
 		String password = pass + salt
+		
 		MessageDigest digest = MessageDigest.getInstance("SHA-256")
 		byte[] hash = digest.digest(password.getBytes("UTF-8"))
-		return printHexBinary(hash);
+		return DatatypeConverter.printHexBinary(hash);
 	}
 	
-	/*public static int byteArrayToInt(byte[] b) {
-		int value = 0;
-		for (int i = 0; i < 4; i++) {
-			int shift = (4 - 1 - i) * 8;
-			value += (b[i] & 0x000000FF) << shift;
-		}
-		return value;
-	}*/
-	
+
 	private static String getSalt() 
 	{
 		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-		byte[] salt = new byte[16];
+		byte[] salt = new byte[32];
 		sr.nextBytes(salt);
 		return salt.toString();
 	}
